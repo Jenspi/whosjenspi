fdca5032c4adbd8f242e59119acb913f2caf8f80
Jennifer (Jenny) Spicer <xJenniferSpicer@gmail.com>
2025-11-24 23:54:17 -0800

    add Awards section

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,9 +2,9 @@
    Copyright (C), 2023-2024, Sara Echeverria (bl33h)
    Author: Sara Echeverria
    FileName: App.jsx
-   Version: I
+   Version: II
    Creation: 02/06/2023
-   Last modification: 02/06/2023
+   Last modification: 11/24/2025 (Jenny Spicer)
 */
 
 import NavBar from "../Components/nav/NavBar";
@@ -13,6 +13,8 @@ import Projects from "../Pages/Projects";
 import About from "../Pages/About";
 import Skills from "../Pages/Skills";
 import Contact from "../Pages/Contact";
+import Awards from "../Pages/Awards";
+//import Research from "../Pages/Research";
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      {/* TODO: <Research /> */}
+      <Awards />
       <Skills />
       <Contact />
     </>
