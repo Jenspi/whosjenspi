7cd3dd205022378cf9ccd9a2f6b9e547e72de34f
Jennifer (Jenny) Spicer <xJenniferSpicer@gmail.com>
2025-11-24 23:55:54 -0800

    add my socials

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -2,9 +2,9 @@
    Copyright (C), 2023-2024, Sara Echeverria (bl33h)
    Author: Sara Echeverria
    FileName: SocialLinks.jsx
-   Version: I
+   Version: II
    Creation: 02/06/2023
-   Last modification: 03/06/2023
+   Last modification: 11/23/2025 (Jenny Spicer)
 */
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -13,22 +13,28 @@ import { icons } from "../Constants/constants";
 const SocialLinks = () => {
   return (
     <div className="w-full h-[100px] bg-grayscale-950 flex justify-center items-center gap-3 p-6">
-      <a href="https://github.com/bl33h" target="_blank">
+      <a href="https://github.com/Jenspi" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-25 hover:text-primary-200 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faGithub}
         />
       </a>
-      <a href="mailto:sarap.echeverria@gmail.com" target="_blank">
+      <a href="mailto:xJenniferSpicer@gmail.com" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-25 hover:text-primary-200 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faEnvelope}
         />
       </a>
-      <a href="https://www.linkedin.com/in/saraech" target="_blank">
+      <a href="https://www.linkedin.com/in/Jenspi" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-25 hover:text-primary-200 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faLinkedin}
+        />
+      </a>
+      <a href="https://jenspi.itch.io" target="_blank">
+        <FontAwesomeIcon
+          className="text-3xl text-grayscale-25 hover:text-primary-200 hover:scale-[1.1] transition-all duration-75"
+          icon={icons.faItchIo}
         />
       </a>
     </div>
