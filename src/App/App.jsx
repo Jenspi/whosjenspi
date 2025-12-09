03bc1568c2fea48a2ebf039ae7e013557445db82
Jennifer (Jenny) Spicer <xJenniferSpicer@gmail.com>
2025-12-09 11:01:26 -0800

    add research component to app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,9 +2,9 @@
    Copyright (C), 2023-2024, Sara Echeverria (bl33h)
    Author: Sara Echeverria
    FileName: App.jsx
-   Version: II
+   Version: III
    Creation: 02/06/2023
-   Last modification: 11/24/2025 (Jenny Spicer)
+   Last modification: 12/09/2025 (Jenny Spicer)
 */
 
 import NavBar from "../Components/nav/NavBar";
@@ -14,7 +14,8 @@ import About from "../Pages/About";
 import Skills from "../Pages/Skills";
 import Contact from "../Pages/Contact";
 import Awards from "../Pages/Awards";
-//import Research from "../Pages/Research";
+import Research from "../Pages/Research";
+//import Blog from "../Pages/Blog";
 
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
-      {/* TODO: <Research /> */}
+      <Research />
       <Awards />
       <Skills />
       {/* TODO: Blog / Game Journals */}
